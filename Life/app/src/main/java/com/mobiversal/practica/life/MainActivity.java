473fc1abc71ffb8dde68d09f09a2a05a144ec48b
moldovan-iancu <iancu271194@yahoo.com>
2017-07-05 13:57:50 +0300

    v2

--- Life/app/src/main/java/com/mobiversal/practica/life/MainActivity.java
+++ Life/app/src/main/java/com/mobiversal/practica/life/MainActivity.java
@@ -1,19 +1,26 @@
 package com.mobiversal.practica.life;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG="MainActivity";
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d( TAG,"onCreate");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,8 +31,47 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d( TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d( TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d( TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d( TAG,"onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d( TAG,"onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d( TAG,"onDestroy");
     }
 
     @Override
