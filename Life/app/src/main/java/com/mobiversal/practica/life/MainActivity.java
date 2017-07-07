013b93dbf6a80f606dbb2b28285f16c2d3c3827b
moldovan-iancu <iancu271194@yahoo.com>
2017-07-07 15:18:27 +0300

    versiunea acc buna

--- Life/app/src/main/java/com/mobiversal/practica/life/MainActivity.java
+++ Life/app/src/main/java/com/mobiversal/practica/life/MainActivity.java
@@ -10,12 +10,19 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="MainActivity";
+    private FirebaseAuth mAuth;
 
     public MainActivity() {
     }
@@ -25,14 +32,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d( TAG,"onCreate");
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef = database.getReference("message").child("1");
 
-        myRef.setValue("Hello, World!");
+        //myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,12 +73,21 @@ public class MainActivity extends AppCompatActivity {
         });
         Intent intent = new Intent(this,SecondActivity.class);
         startActivity(intent );
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d( TAG,"onStart");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null)
+        {
+            Intent startIntent =new Intent(MainActivity.this,StartActivity.class);
+            startActivity(startIntent);
+            finish();
+
+        }
     }
 
     @Override
