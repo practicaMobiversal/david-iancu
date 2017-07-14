b9a1d7cc471f54b11002eda32d4faeb8ba620d8d
moldovan-iancu <iancu271194@yahoo.com>
2017-07-14 10:16:50 +0300

    v2 chat buna

--- Life/app/src/main/java/com/mobiversal/practica/life/UserActiviy.java
+++ Life/app/src/main/java/com/mobiversal/practica/life/UserActiviy.java
@@ -9,11 +9,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -21,7 +19,7 @@ import com.squareup.picasso.Picasso;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserActiviy extends AppCompatActivity {
-        private Toolbar mToolbar;
+    private Toolbar mToolbar;
     private RecyclerView mUsersList;
     private DatabaseReference mUsersDatabase;
 
@@ -39,83 +37,65 @@ public class UserActiviy extends AppCompatActivity {
 
         mUsersDatabase= FirebaseDatabase.getInstance().getReference().child("users");
 
-
         mUsersList=(RecyclerView) findViewById(R.id.users_list);
         mUsersList.setHasFixedSize(true);
         mUsersList.setLayoutManager(new LinearLayoutManager(this));
 
-        FirebaseRecyclerAdapter<Show_Chat_Activity_Data_Items,UsersViewHolder> firebaseRecyclerAdapter=
-                new FirebaseRecyclerAdapter<Show_Chat_Activity_Data_Items, UsersViewHolder>(Show_Chat_Activity_Data_Items.class, R.layout.users_single, UsersViewHolder.class,mUsersDatabase ) {
-                    @Override
-                    public UsersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                        UsersViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
-
-                        viewHolder.setOnClickListener(new UsersViewHolder.ClickListener() {
-                            @Override
-                            public void onItemClick(View v, int position) {
-                                Intent profileIntent = new Intent (UserActiviy.this,ProfileActivity.class);
-                                final String user_id = getRef(position).getKey();
-                                profileIntent.putExtra("user_id", user_id);
-                                startActivity(profileIntent);
-                            }
-                        });
-
-            protected void populateViewHolder(UsersViewHolder viewHolder, Users users, int position) { @Override
-            protected void populateViewHolder(UsersViewHolder viewHolder, Show_Chat_Activity_Data_Items users, int position) {
-
- viewHolder.setName(users.getName());
-                viewHolder.setImage(users.gettumbimg(),getApplicationContext());
-
-                final String user_id=getRef(position).getKey();
-                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
-                      @Override
-                    protected void populateViewHolder(UsersViewHolder viewHolder, Users users, int position) {
-
- viewHolder.nameView.setText(users.getName());
-                        Picasso.with(getApplicationContext())
-                                .load(users.gettumbimg())
-                                .placeholder(R.drawable.default_user)
-                                .into(viewHolder.circImageView);                    }
-                };
-        mUsersList.setAdapter(firebaseRecyclerAdapter);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        FirebaseRecyclerAdapter<Show_Chat_Activity_Data_Items,UsersViewHolder> firebaseRecyclerAdapter=
+                new FirebaseRecyclerAdapter<Show_Chat_Activity_Data_Items, UsersViewHolder>(Show_Chat_Activity_Data_Items.class, R.layout.users_single, UsersViewHolder.class,mUsersDatabase ) {
 
 
+                    @Override
+                    protected void populateViewHolder(UsersViewHolder viewHolder, Show_Chat_Activity_Data_Items users, int position) {
 
+                        viewHolder.setName(users.getName());
+                        viewHolder.setImage(users.gettumbimg(),getApplicationContext());
+
+                        final String user_id=getRef(position).getKey();
+                        viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent profileIntent = new Intent (UserActiviy.this,ProfileActivity.class);
+                                profileIntent.putExtra("user_id",user_id);
+                                startActivity(profileIntent);
+
+                            }
+                        });
+
+                    }
+                };
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
     }
 
 
 
     public static class UsersViewHolder extends RecyclerView.ViewHolder{
-        TextView nameView;
-        CircleImageView circImageView;
-
-        private UsersViewHolder.ClickListener myListener;
-
-        public interface ClickListener {
-            public void onItemClick(View view, int position);
-        }
+        View mView;
 
         public UsersViewHolder(View itemView) {
             super(itemView);
-            nameView = (TextView) itemView.findViewById(R.id.user_single_name);
-            circImageView = (CircleImageView) itemView.findViewById(R.id.single_user_img);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    myListener.onItemClick(v, getAdapterPosition());
-                }
-            });
-       }
+            mView=itemView;
 
-       public void setOnClickListener(ClickListener clickListener) {
-           myListener = clickListener;
-       }
+        }
+
+        public void setName(String name){
+
+            TextView mUserName= (TextView) mView.findViewById(R.id.user_single_name);
+            mUserName.setText(name);
+        }
+
+        public void setImage(String tumb_image, Context ct){
+
+            CircleImageView userImageView=(CircleImageView) mView.findViewById(R.id.circleImageView) ;
+            Picasso.with(ct).load(tumb_image).placeholder(R.drawable.default_user).into(userImageView);
+
+        }
     }
 
 
--- Life/app/src/main/java/com/mobiversal/practica/life/Users.java
+++ Life/app/src/main/java/com/mobiversal/practica/life/Users.java
@@ -1,3 +1,4 @@
+
 //package com.mobiversal.practica.life;
 //
 //
